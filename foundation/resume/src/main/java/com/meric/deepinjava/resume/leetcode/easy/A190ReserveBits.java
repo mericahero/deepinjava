4dd70ed79c5345d3e332aaf111c298d2d268a9ef
meric <aheroaa@gmail.com>
2020-04-02 17:26:10 +0800

    resume

--- foundation/resume/src/main/java/com/meric/deepinjava/resume/leetcode/easy/A190ReserveBits.java
+++ foundation/resume/src/main/java/com/meric/deepinjava/resume/leetcode/easy/A190ReserveBits.java
@@ -3,7 +3,7 @@ package com.meric.deepinjava.resume.leetcode.easy;
 public class A190ReserveBits {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-
+        return 0;
     }
 
     public static void main(String[] args) {
